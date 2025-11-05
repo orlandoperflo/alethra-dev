c686257278dc966e5c4b698e0bac271144e1a1c7
orlandoperflo <69471535+orlandoperflo@users.noreply.github.com>
2025-11-04 22:55:18 -0600

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,3 +1,10 @@
+// NAV
+fetch("/nav.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("nav-placeholder").innerHTML = html;
+    initNavLogic(); // your nav js activation
+  });
 
 // FOOTER
 fetch("/footer.html")
